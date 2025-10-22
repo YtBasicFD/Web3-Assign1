265cc808b75e7b31321eaa60e52953130d1f0c73
YtBasicFD <90427532+YtBasicFD@users.noreply.github.com>
2025-10-22 05:47:41 +0000

    created and tested apis for circuits in lab14b, made sure they worked and returned what I believe is the correct information. Added them here after

--- f1-server.js
+++ f1-server.js
@@ -5,4 +5,37 @@ const app = express();
 const supaUrl = 'https://bbutseotcterrdjqznrt.supabase.co';
 const supaAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJidXRzZW90Y3RlcnJkanF6bnJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5OTMzNzUsImV4cCI6MjA3NjU2OTM3NX0.QK9kW10YS2IoB3TqkXwCiRGZKFnwz0Nmw_Cn4AnGlHI';
 
-const supabase = supa.createClient(supaUrl, supaAnonKey); 
+const supabase = supa.createClient(supaUrl, supaAnonKey);
+
+app.use(express.json());
+
+//circuits api
+app.get("/api/circuits", async (req, res) => {
+  const { data, error } = await supabase
+  .from("circuits")
+  .select("*");
+
+  res.json(data);
+});
+
+app.get("/api/circuits/:ref", async (req, res) => {
+  const { ref } = req.params;
+  const { data, error } = await supabase
+  .from("circuits")
+  .select("*")
+  .eq("circuitRef", ref)
+  .single();
+
+  res.json(data);
+});
+
+app.get("/api/circuits/season/:year", async (req, res) => {
+  const { year } = req.params;
+  const { data, error } = await supabase
+    .from("races")
+    .select("round, circuits!inner(*)")
+    .eq("year", year)
+    .order("round", { ascending: true });
+
+  res.json(data);
+});
